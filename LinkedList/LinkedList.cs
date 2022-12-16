f0b15d7feba7d91f920c6515a1b2527356da37a7
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-17 00:27:03 +0530

    [Prashant]:Linked list by Appending

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine($"Added {node.data} to the list");
+        }
+        public void AddFront(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = null;
+            else
+            {
+                node.next = this.head;
+                this.head = node;
+            }
+            Console.WriteLine($"Added {node.data} to the front");
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
+    
+
---
+++ LinkedList/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(70);
+            list.AddFront(30);
+            list.AddFront(56);
+            list.Display();
+        }
+    }
+}
