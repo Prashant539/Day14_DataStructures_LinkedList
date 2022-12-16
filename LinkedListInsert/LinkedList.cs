fb17d0a7683f6065a72982bfb603b5edf2e626b6
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-17 00:55:05 +0530

    [Prashant]:Ability to insert

---
+++ LinkedListInsert/LinkedList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListInsert
+{
+
+    public class LinkedList
+    {
+        public Node head;
+
+        public void add(int data)
+        {
+            Node node = new Node(data);
+            node.data = data;
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+
+                }
+                temp.next = node;
+            }
+
+        }
+
+        public void AddBTW(int index, int data)
+        {
+            Node node = new Node(data);
+            node.data = data;
+
+            Node temp = head;
+            for (int i = 0; i < index - 1; i++)
+            {
+                temp = temp.next;
+            }
+            node.next = temp.next;
+            temp.next = node;
+
+        }
+        public void show()
+        {
+            Node node = head;
+            while (node.next != null)
+            {
+                Console.WriteLine(node.data);
+                node = node.next;
+            }
+            Console.WriteLine(node.data);
+        }
+    }
+    
+}
+
---
+++ LinkedListInsert/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace LinkedListInsert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.add(56);
+            list.add(70);
+            list.AddBTW(1, 30);
+            list.show();
+        }
+    }
+}
