28ae755ba7d2abb7bce372f1c585f3194b7b7b13
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-17 00:22:31 +0530

    [Prashant]:Create a simple linked list

---
+++ DataStructure/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructure
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList likedList = new LinkedList();
+            likedList.Add(56);
+            likedList.Add(30);
+            likedList.Add(70);
+        }
+    }
+}   
+            
+
+   
+    
