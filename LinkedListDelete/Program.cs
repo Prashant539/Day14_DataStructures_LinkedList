860660ce7156369c5adcd371d96fbb4ff877af81
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-22 23:26:46 +0530

    [Prashant]:Delete the first element

---
+++ LinkedListDelete/Program.cs
@@ -0,0 +1,25 @@
+﻿using LinkedListDelete;
+using System;
+using System.Collections.Generic;
+
+namespace linkedListDelete
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Operations");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            //list.Add(30);
+            list.Add(70);
+            list.Display();
+            list.InsertAtParticularPosition(2, 30);
+            list.RemoveFirstNode();
+            list.Display();
+        }
+    }
+}
+        
+    
+
