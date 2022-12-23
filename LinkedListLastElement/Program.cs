e4253052214784e56469123b6a1fa7d37553ac9f
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-23 21:59:49 +0530

    [Prashant]: Delete the last element

---
+++ LinkedListLastElement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListLastElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Operations");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            //list.Add(30);
+            list.Add(70);
+            list.Display();
+            list.InsertAtParticularPosition(2, 30);
+            list.RemoveFirstNode();
+            list.Display();
+            list.RemoveLastNode();
+            list.Display();
+        }
+    }
+}
